c183a2722b6a810473f5caca2b0e8438c07bb35e
lakekimhandsome <115394409+lakekimhandsome@users.noreply.github.com>
2025-04-30 22:46:18 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-const ball = document.getElementById("ball");
+const ball1 = document.getElementById("ball1");
+const ball2 = document.getElementById("ball2");
 const sound = document.getElementById("sound");
 
 function getRandomPosition() {
-  const windowWidth = window.innerWidth - ball.offsetWidth;
-  const windowHeight = window.innerHeight - ball.offsetHeight;
+  const windowWidth = window.innerWidth - ball1.offsetWidth;
+  const windowHeight = window.innerHeight - ball1.offsetHeight;
 
   const randomX = Math.random() * windowWidth;
   const randomY = Math.random() * windowHeight;
@@ -12,13 +13,21 @@ function getRandomPosition() {
 }
 
 function moveBall() {
-  const { x, y } = getRandomPosition();
-  ball.style.left = `${x}px`;
-  ball.style.top = `${y}px`;
+  const { x: x1, y: y1 } = getRandomPosition();
+  const { x: x2, y: y2 } = getRandomPosition();
+  ball1.style.left = `${x1}px`;
+  ball1.style.top = `${y1}px`;
+  ball2.style.left = `${x2}px`;
+  ball2.style.top = `${y2}px`;
 }
 
 // 공을 터치했을 때 사운드 재생 (이미 재생 중이라도 처음부터 다시 재생)
-ball.addEventListener("click", () => {
+ball1.addEventListener("click", () => {
+  sound.currentTime = 0; // 사운드를 처음부터 다시 재생
+  sound.play();
+});
+
+ball2.addEventListener("click", () => {
   sound.currentTime = 0; // 사운드를 처음부터 다시 재생
   sound.play();
 });

bdff0cbacc41ee3f8e21136678e37fac093db2f6
lakekimhandsome <115394409+lakekimhandsome@users.noreply.github.com>
2025-04-30 23:14:32 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const ball1 = document.getElementById("ball1");
 const ball2 = document.getElementById("ball2");
 const sound = document.getElementById("sound");
+const showBallCheckbox = document.getElementById("showBallCheckbox");
+var isSecondBallShowed = 0;
 
 function getRandomPosition() {
   const windowWidth = window.innerWidth - ball1.offsetWidth;
@@ -27,9 +29,21 @@ ball1.addEventListener("click", () => {
   sound.play();
 });
 
-ball2.addEventListener("click", () => {
-  sound.currentTime = 0; // 사운드를 처음부터 다시 재생
-  sound.play();
+if (isSecondBallShowed) {
+  ball2.addEventListener("click", () => {
+    sound.currentTime = 0; // 사운드를 처음부터 다시 재생
+    sound.play();
+  });
+}
+
+showBallCheckbox.addEventListener("change", () => {
+  if (showBallCheckbox.checked) {
+    ball2.style.display = "block"; // 체크되면 두 번째 공 보이게
+    isSecondBallShowed = 1;
+  } else {
+    ball2.style.display = "none"; // 체크 해제되면 두 번째 공 숨기기
+    isSecondBallShowed = 0;
+  }
 });
 
 // 일정 시간마다 공이 랜덤 위치로 부드럽게 이동

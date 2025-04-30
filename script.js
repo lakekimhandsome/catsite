d8fba68b009463ef983d6d2d873dca14f32859ca
lakekimhandsome <115394409+lakekimhandsome@users.noreply.github.com>
2025-04-30 22:18:13 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const ball = document.getElementById("ball");
+const sound = document.getElementById("sound");
 
 function getRandomPosition() {
   const windowWidth = window.innerWidth - ball.offsetWidth;
@@ -15,6 +16,11 @@ function moveBall() {
   ball.style.left = `${x}px`;
   ball.style.top = `${y}px`;
 }
+
+// 공을 터치했을 때 사운드 재생
+ball.addEventListener("click", () => {
+  sound.play();
+});
 
 // 일정 시간마다 공이 랜덤 위치로 부드럽게 이동
 setInterval(moveBall, 1000);
